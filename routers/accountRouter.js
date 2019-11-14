85f2c4722129b06e59d529116bb51edb54a982ce
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-13 20:17:32 -0700

    added more validation, post, and put

--- routers/accountRouter.js
+++ routers/accountRouter.js
@@ -27,6 +27,40 @@ router.get("/:id", validateID, (req, res) => {
     });
 });
 
+router.post("/", validateBody, validateAccountKeys, (req, res) => {
+  const bodyInfo = req.body;
+
+  db.insert(bodyInfo)
+    .into("accounts")
+    .then(acct => {
+      res.status(201).json(acct);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error adding new account", err });
+    });
+});
+
+router.put(
+  "/:id",
+  validateID,
+  validateBody,
+  validateAccountKeys,
+  (req, res) => {
+    const id = req.params.id;
+    const bodyInfo = req.body;
+
+    db("accounts")
+      .where({ id })
+      .update(bodyInfo)
+      .then(acct => {
+        res.status(200).json(acct);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Error updating account", err });
+      });
+  }
+);
+
 // doot doot magic floot
 // middleware lives here
 // these comments are just creating space
@@ -41,17 +75,33 @@ function validateID(req, res, next) {
     .then(acct => {
       acct[0]
         ? next()
-        : res
-            .status(404)
-            .json({
-              message: `Account with ID ${req.params.id} can't be found`
-            });
+        : res.status(404).json({
+            message: `Account with ID ${req.params.id} can't be found`
+          });
     })
     .catch(err => {
       res
         .status(500)
         .json({ message: "Error validating account ID to be returned", err });
     });
+}
+
+function validateBody(req, res, next) {
+  const bodyInfo = req.body;
+
+  Object.keys(bodyInfo).length > 0
+    ? next()
+    : res.status(400).json({ message: "Missing request body" });
+}
+
+function validateAccountKeys(req, res, next) {
+  const bodyInfo = req.body;
+
+  bodyInfo.name && bodyInfo.budget
+    ? next()
+    : res
+        .status(400)
+        .json({ message: "Request body missing name or budget key" });
 }
 
 module.exports = router;
